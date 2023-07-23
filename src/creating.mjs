73bf77ad31274c3e703a4e75184bbaad2270e476
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2023-07-23 19:00:52 -0300

    feat: implement interval() function

--- src/creating.mjs
+++ src/creating.mjs
@@ -1,4 +1,4 @@
-import setText from "./results.mjs";
+import setText, { appendText } from "./results.mjs";
 
 export function timeout() {
   const wait = new Promise((resolve) => {
@@ -10,7 +10,19 @@ export function timeout() {
   wait.then((text) => setText(text));
 }
 
-export function interval() {}
+export function interval() {
+  let counter = 0;
+  const wait = new Promise((resolve) => {
+    setInterval(() => {
+      console.log("INTERVAL");
+      resolve(`Timeout! ${++counter}`);
+    }, 1500);
+  });
+
+  wait
+    .then((text) => setText(text))
+    .finally(() => appendText(` -- Done ${counter}`));
+}
 
 export function clearIntervalChain() {}
 

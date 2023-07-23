9502c94597cfaf2dd62b096a318af3540ba74186
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2023-07-23 19:15:32 -0300

    feat: implement xhr() function

--- src/creating.mjs
+++ src/creating.mjs
@@ -34,12 +34,26 @@ export function clearIntervalChain() {
     }, 1500);
   });
 
-  wait
-    .then((text) => setText(text))
-    .finally(() => clearInterval(interval));
+  wait.then((text) => setText(text)).finally(() => clearInterval(interval));
 }
 
-export function xhr() {}
+export function xhr() {
+  let request = new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost:3000/users/7");
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            resolve(xhr.responseText);
+        } else {
+            reject(xhr.statusText);
+        }
+    }
+    xhr.onerror = () => reject("Request Failed!");
+    xhr.send();
+  });
+
+  request.then((result) => setText(result)).catch((reason) => setText(reason));
+}
 
 export function allPromises() {}
 

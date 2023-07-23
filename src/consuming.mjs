24577121beab70962665de6edaeff492db747a9e
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2023-07-23 18:32:11 -0300

    feat: implement final() function

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,4 +1,4 @@
-import setText /*, {appendText, showWaiting, hideWaiting}*/ from "./results.mjs";
+import setText, { appendText, hideWaiting, showWaiting } from "./results.mjs";
 
 export function get() {
   axios.get("http://localhost:3000/orders/1").then(({ data }) => {
@@ -32,7 +32,9 @@ export function chainCatch() {
   axios
     .get("http://localhost:3000/orders/1")
     .then(({ data }) => {
-      return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+      return axios.get(
+        `http://localhost:3000/addresses/${data.shippingAddress}`
+      );
     })
     .then(({ data }) => {
       setText(`City: ${data.city}`);
@@ -40,4 +42,24 @@ export function chainCatch() {
     .catch((err) => setText(err));
 }
 
-export function final() {}
+export function final() {
+  showWaiting();
+  axios
+    .get("http://localhost:3000/orders/1")
+    .then(({ data }) => {
+      return axios.get(
+        `http://localhost:3000/addresses/${data.shippingAddress}`
+      );
+    })
+    .then(({ data }) => {
+      setText(`City: ${data.city}`);
+    })
+    .catch((err) => setText(err))
+    .finally(() => {
+      setTimeout(() => {
+        hideWaiting();
+      }, 1500);
+
+      appendText(" -- Completely Done!");
+    });
+}

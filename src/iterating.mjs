43a5ab6d7063781872d95d9ccffcb735a8b4a1d3
Giuliani Deon Sanches <giulianisanches@users.noreply.github.com>
2023-07-25 18:41:44 -0300

    feat: implement getCatch function

--- src/iterating.mjs
+++ src/iterating.mjs
@@ -1,18 +1,21 @@
-import setText from './results.mjs';
+import setText from "./results.mjs";
 
-export async function get(){
-    const { data } = await axios.get("http://localhost:3000/orders/1");
+export async function get() {
+  const { data } = await axios.get("http://localhost:3000/orders/1");
+  setText(JSON.stringify(data));
+}
+
+export async function getCatch() {
+  try {
+    const { data } = await axios.get("http://localhost:3000/orders/123");
     setText(JSON.stringify(data));
+  } catch (error) {
+    setText(error);
+  }
 }
 
-export function getCatch(){
-}
+export function chain() {}
 
-export function chain(){
-}
+export function concurrent() {}
 
-export function concurrent(){
-}
-
-export function parallel(){
-}
+export function parallel() {}
